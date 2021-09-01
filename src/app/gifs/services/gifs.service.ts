d1eb7808e152a614c21d7451046d3b8125435325
Asier Garcia Perez <asiergarper@gmail.com>
2021-09-01 11:05:02 +0200

    Implementada funcionalidad de busqueda de gifs con Api en Service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGifsResponse, Images, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'RkmWI2yWs6l1cRYBCoYfYVLlRpYyvrMI'
   private _historial: string[] = [];
+
+  //ToDo: Cambiar any por su tipo
+  public resultados: Gif[] = [];
 
 
   get historial(){
     return [...this._historial];
   }
 
+  constructor( private http: HttpClient){
+
+  }
   buscarGifs( query: string) {
 
-    this._historial.unshift(query);
+    query = query.trim().toLocaleLowerCase();
+    if( !this._historial.includes( query)){
+      this._historial.unshift(query);
+      this._historial= this._historial.splice(0, 10)
+    }
 
-    console.log(this._historial);
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=RkmWI2yWs6l1cRYBCoYfYVLlRpYyvrMI&q=${query}&limit=10`)
+    .subscribe((resp) => {
+      console.log(resp.data);
+      this.resultados = resp.data;
+    });
+   
     
   }
 }
